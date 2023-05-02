592f1f1751fc46969f0c2e548357fd016388cf8a
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-05-02 10:35:46 +0600

    module_14.5

--- Module - 14.5 Practice Day/W_Shape_3.c
+++ Module - 14.5 Practice Day/W_Shape_3.c
@@ -2,33 +2,39 @@
 
 int main()
 {
-    int n, k, s;
+    int n;
     scanf("%d", &n);
-    s = n - 1;
-    k = 1;
-    for (int i = 1; i <= 2 * n; i++)
+    int s = (2 * n - 1) / 2;
+  
+    for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= s; j++)
         {
             printf(" ");
         }
-        for (int j = 1; j <= k; j++)
+
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
             printf("*");
         }
- 
-        if (i <= n -1)
-        {
-            s--;
-            k = k + 2;
-        }
-      
-        else
-        {
-            s++;
-            k = k - 2;
-        }
+
         printf("\n");
+        s--;
+    }
+    s = 0;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        s++;
     }
 
     return 0;
