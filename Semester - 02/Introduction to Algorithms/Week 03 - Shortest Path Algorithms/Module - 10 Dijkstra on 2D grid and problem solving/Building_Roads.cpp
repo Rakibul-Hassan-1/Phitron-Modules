0249111b087d3fc3fb824052c3c1d7d1f0fbcf7b
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-08-14 22:13:22 +0600

    new

---
+++ Semester - 02/Introduction to Algorithms/Week 03 - Shortest Path Algorithms/Module - 10 Dijkstra on 2D grid and problem solving/Building_Roads.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+vector<int> adj[N];
+bool visited[N];
+
+void DFS(int u)
+{
+    visited[u] = true;
+    for (int v : adj[u])
+    {
+        if (visited[v])
+            continue;
+
+        DFS(v);
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<int> leaders;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited[i])
+            continue;
+        leaders.push_back(i);
+        DFS(i);
+    }
+    cout << leaders.size() - 1 << endl;
+
+    for (int i = 0; i < leaders.size() - 1; i++)
+    {
+        cout << leaders[i] << " " << leaders[i + 1] << endl;
+    }
+
+    return 0;
+}
---
+++ Semester - 02/Introduction to Algorithms/Week 03 - Shortest Path Algorithms/Module - 10 Dijkstra on 2D grid and problem solving/Labyrinth.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e3 + 10;
+int visited[N][N];
+vector<string> g;
+typedef pair<int, int> pii;
+vector<pii> direction = {{0, -1}, {0, +1}, {-1, 0}, {+1, 0}};
+int n, m;
+
+bool isValid(int i, int j)
+{
+    return (i >= 0 && i < n && j >= 0 && j < m);
+}
+
+void dfs(int i, int j)
+{
+    // validity cheak
+    if (!isValid(i, j))
+        return;
+
+    if (visited[i][j])
+        return;
+
+    if (g[i][j] == '#')
+        return;
+
+    visited[i][j] = true;
+
+    for (auto d : direction)
+    {
+        dfs(i + d.first, j + d.second);
+    }
+}
+
+int main()
+{
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        string x;
+        cin >> x;
+        g.push_back(x);
+    }
+
+    int room = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (g[i][j] == '#')
+                continue;
+
+            if (visited[i][j])
+                continue;
+
+            dfs(i, j);
+            room++;
+        }
+    }
+    cout << room << endl;
+
+    return 0;
+}
