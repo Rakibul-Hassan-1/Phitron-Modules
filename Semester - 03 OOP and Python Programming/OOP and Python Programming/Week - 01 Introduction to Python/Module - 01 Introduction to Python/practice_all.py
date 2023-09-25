3661049c3e6b7b6ae371002cd7510bc4d01ba8a1
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-09-25 11:10:20 +0600

    new

---
+++ Semester - 03 OOP and Python Programming/OOP and Python Programming/Week - 01 Introduction to Python/Module - 01 Introduction to Python/practice_all.py
@@ -0,0 +1,14 @@
+# name = ['Rakib', 'Shohan', "Fyashal", 'Akif', 'Akib']
+# for x in name:
+#     print(x)
+
+# array = []
+# value = input('Enter the values: ')
+# for i in range(0,10):
+#     print("Hello world")
+
+# Printing index of Array. 
+my_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I']
+for i in range(0,len(my_list), 1):
+    ans = f"Index {i} : {my_list[i]}"
+    print(ans)
